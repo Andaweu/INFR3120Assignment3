4f545de5a24a3e37e3e70c983b090a496dfd6513
Andaweu <andrew.chester@ontariotechu.net>
2025-11-25 22:11:33 -0500

    Worked on style sheet and formatting

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const methodOverride = require('method-override')
 
 var mongoDB_URI = process.env.MONGODB_URI
 
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
